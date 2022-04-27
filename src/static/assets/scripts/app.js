4721503464620c8583d895631dcf21f5ca2805aa
DESKTOP-OGLLSNF\AGL <nguyen_xuan_hiep@allgrow-labo.jp>
2022-04-27 11:02:53 +0700

    final commit

--- src/static/assets/scripts/app.js
+++ src/static/assets/scripts/app.js
@@ -2,15 +2,27 @@ let $ = document.querySelector.bind(document);
 let $$ = document.querySelectorAll.bind(document);
 let header = $(".c-header");
 let menuMobile = $("#menuMobile");
-let introContents = $$(".p-intro__content > dd");
+let introContents = $$(".p-intro__content > .p-intro__info");
 let showInfos = $$(".p-intro__showInfo");
+let pointList1 = $$(".p-point__list1 > .p-point__wrapItem");
+let pointList2 = $$(".p-point__list2 > .p-point__wrapItem");
+let pointList3 = $$(".p-point__list3 > .p-point__wrapItem");
+let pointList1Btn = $$(
+  ".p-point__list1 > .p-point__wrapItem > .p-point__Item > .p-point__btnMore"
+);
+let pointList2Btn = $$(
+  ".p-point__list2 > .p-point__wrapItem > .p-point__Item > .p-point__btnMore"
+);
+let pointList3Btn = $$(
+  ".p-point__list3 > .p-point__wrapItem > .p-point__Item > .p-point__btnMore"
+);
 
 window.onscroll = function () {
   handleScrollTopHeader();
 };
 
 function handleScrollTopHeader() {
-  var scrollTopHeader =
+  let scrollTopHeader =
     window.pageYOffset ||
     document.documentElement.scrollTop ||
     document.body.scrollTop ||
@@ -29,5 +41,38 @@ menuMobile.onclick = function () {
 for (let i = 0; i < showInfos.length; i++) {
   showInfos[i].onclick = function () {
     introContents[i].classList.toggle("is-show");
+  };
+}
+
+let clearPointList1 = function () {
+  for (let i = 0; i < pointList1.length; i++) {
+    pointList1[i].classList.remove("active");
+  }
+};
+let clearPointList2 = function () {
+  for (let i = 0; i < pointList2.length; i++) {
+    pointList2[i].classList.remove("active");
+  }
+};
+let clearPointList3 = function () {
+  for (let i = 0; i < pointList3.length; i++) {
+    pointList3[i].classList.remove("active");
+  }
+};
+
+for (let i = 0; i < pointList1.length; i++) {
+  pointList1Btn[i].onclick = function () {
+    clearPointList1();
+    pointList1[i].classList.add("active");
+  };
+
+  pointList2Btn[i].onclick = function () {
+    clearPointList2();
+    pointList2[i].classList.add("active");
+  };
+
+  pointList3Btn[i].onclick = function () {
+    clearPointList3();
+    pointList3[i].classList.add("active");
   };
 }
